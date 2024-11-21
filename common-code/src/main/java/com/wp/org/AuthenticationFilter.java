c75b52234bec7d40d37e0aa0f08e62019896d791
Aditi Joshi <joshiadisha2001@gmail.com>
2024-11-21 09:56:37 +0530

    Wallet Pay Push all code

---
+++ common-code/src/main/java/com/wp/org/AuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.wp.org;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthenticationFilter extends OncePerRequestFilter {
+
+    private final RestTemplate restTemplate=new RestTemplate();
+    private final String userServiceAuthUrl="http://localhost:8080/user-service/validate";
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+    	String requestPath = request.getRequestURI();
+    	if (requestPath.equals("/user-service/user") || requestPath.startsWith("/user-service/validate") || requestPath.startsWith("/transaction-service/status") || requestPath.startsWith("/wallet-service/payment-status-capture")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
+
+        if (authHeader == null || !authHeader.startsWith("Basic ")) {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+            return;
+        }
+
+            String base64Credentials = authHeader.substring(6);
+            String credentials = new String(Base64.getDecoder().decode(base64Credentials), StandardCharsets.UTF_8);
+            String[] values = credentials.split(":", 2);
+
+            LoginRequest loginRequest = new LoginRequest();
+            loginRequest.setUsername(values[0]);
+            loginRequest.setPassword(values[1]);
+            HttpEntity<LoginRequest> requestEntity = new HttpEntity<>(loginRequest);
+
+            ResponseEntity<String> authResponse = restTemplate.postForEntity(userServiceAuthUrl, requestEntity, String.class);
+
+            if (!authResponse.getStatusCode().is2xxSuccessful()) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Invalid credentials");
+                return;
+            }
+
+            filterChain.doFilter(request, response);
+    }
+}
